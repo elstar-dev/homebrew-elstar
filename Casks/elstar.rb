a7c7ab70e989a6f1c12ae2a70e30bbed8feadc3d
Patrick <patrick@ahrentlov.io>
2026-07-25 11:57:45 +0200

    Point download URL at elstar-dev/elstar releases (assets mirrored, sha unchanged)

--- Casks/elstar.rb
+++ Casks/elstar.rb
@@ -2,7 +2,7 @@ cask "elstar" do
   version "1.0.1"
   sha256 "39bc471d0ec53ab7e6dc0828520b3bf2265ef8dd21223d6c9610a467506c2389"
 
-  url "https://github.com/Ahrentlov/homebrew-elstar/releases/download/v#{version}/elstar-#{version}-macos.tar.gz"
+  url "https://github.com/elstar-dev/elstar/releases/download/v#{version}/elstar-#{version}-macos.tar.gz"
   name "elstar"
   desc "iOS Simulator development & automation toolkit"
   homepage "https://elstar.dev"
