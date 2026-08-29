813e114b2f10c48225b246b48acbc29acb794890
Patrick <patrick@ahrentlov.io>
2026-08-29 23:37:43 +0200

    elstar 1.0.4

--- Casks/elstar.rb
+++ Casks/elstar.rb
@@ -1,6 +1,6 @@
 cask "elstar" do
-  version "1.0.3"
-  sha256 "621aa666839435febd794eb276760696d7b53665b71a235040ac00ae359dbef9"
+  version "1.0.4"
+  sha256 "dbd8bb97f3c83e1c7c76de5e38dab803f73be3be3056faa45792505c28ef51ee"
 
   url "https://github.com/elstar-dev/elstar/releases/download/v#{version}/elstar-#{version}-macos.tar.gz"
   name "elstar"
